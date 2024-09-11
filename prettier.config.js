d0f5213309f544f5e5dd852b91cdc992ea0a52e2
bullbazaar <bullbazaar@proton.me>
2024-09-11 12:34:02 +0700

    chore: update prettier, add config

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  printWidth: 150,
+  singleQuote: true,
+  quoteProps: 'preserve',
+};
