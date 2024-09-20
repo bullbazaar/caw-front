7c0f227edf34c2c8465968e6144be8bf718b1e77
bullbazaar <bullbazaar@proton.me>
2024-09-20 21:15:00 +0700

    fix: make landing block animation seamless, optimize more

--- src/components/landing/CardParallaxSection.tsx
+++ src/components/landing/CardParallaxSection.tsx
@@ -1,7 +1,7 @@
 import { Box, chakra, Flex, Heading, Stack, VStack, Text, useBreakpointValue, Spacer, useColorModeValue, Tag, TagLabel } from '@chakra-ui/react';
-import { m } from 'framer-motion';
+import { AnimatePresence, m } from 'framer-motion';
 
-import { ParallaxText, ParallaxItemWrapper } from 'src/components/animate/ParallaxText';
+import { ParallaxText } from 'src/components/animate/ParallaxText';
 import { varFade } from 'src/components/animate';
 
 import { FlippingText } from './FlippingText';
@@ -18,7 +18,7 @@ function Card({ data }: { data: DataModel }) {
         minWidth={CARD_WIDTH}
         maxWidth={CARD_WIDTH}
         minHeight={CARD_HEIGHT}
-        maxHeight={CARD_HEIGHT}
+        maxHeight="30vh"
         colorScheme={data.colorSchema}
         variant="subtle"
         letterSpacing={'0.1em'}
@@ -42,70 +42,82 @@ export default function CardParallaxSection() {
 
   return (
     <>
-      <div
-        id="title-and-text-about-caw-manifesto"
-        className="landing-title-fade-in pointer-events-none absolute z-10 hidden w-full p-2 text-center md:block"
-      >
-        <Flex w={'full'} h={'container.lg'} p={2} backgroundPosition={'center center'}>
-          <VStack w={'full'} justify={'center'} px={px} p={2}>
-            <Stack maxW={'2xl'} align={'flex-start'} spacing={6} p={2}>
-              <Heading as="h1" fontSize="9xl" fontWeight="extrabold" zIndex={3} color={'white'} p={2}>
-                <Box w="100%" alignSelf="center">
-                  <m.div variants={varFade().inUp}>
-                    <ContentAlign>
-                      <Text
-                        id="decentralized-social-clearing-house"
-                        color={textColor}
-                        fontSize="6xl"
-                        fontWeight="bold"
-                        textAlign={'center'}
-                        zIndex={7}
-                      >
-                        A
-                        <br />
-                        <DecentralizedTextStyle>DECENTRALIZED</DecentralizedTextStyle>
-                        <br />
-                        SOCIAL CLEARING HOUSE
-                      </Text>
-                    </ContentAlign>
-                  </m.div>
-                  <Spacer h={10} />
-                  <m.div variants={varFade().in}>
-                    <chakra.div paddingLeft={{ base: 0, md: '30%' }}>
-                      <FlippingText textColor={textColor} />
-                    </chakra.div>
-                  </m.div>
-                  <br />
-                </Box>
-              </Heading>
-            </Stack>
-          </VStack>
-        </Flex>
-      </div>
-      <Box id="social-issues-section" className="mt-[-130px] overflow-hidden">
-        <div className="landing-cards-zoom-in hidden md:block">
-          <ParallaxText baseVelocity={isMd ? 1 : 0}>
-            <ParallaxItemWrapper id="pax-group-1">
-              {dataSet1.map((item) => (
-                <Card key={item.id} data={item} />
-              ))}
-            </ParallaxItemWrapper>
-          </ParallaxText>
-          <ParallaxText baseVelocity={isMd ? -1 : 0}>
-            <ParallaxItemWrapper id="pax-group-2">
-              {dataSet2.map((item) => (
-                <Card key={item.id} data={item} />
-              ))}
-            </ParallaxItemWrapper>
-          </ParallaxText>
-          <ParallaxText baseVelocity={isMd ? 0.5 : 0}>
-            <ParallaxItemWrapper id="pax-group-3">
-              {dataSet3.map((item) => (
-                <Card key={item.id} data={item} />
-              ))}
-            </ParallaxItemWrapper>
-          </ParallaxText>
-        </div>
+      <AnimatePresence>
+        {isMd && (
+          <div
+            id="title-and-text-about-caw-manifesto"
+            className="landing-title-fade-in pointer-events-none absolute z-10 w-full p-2 pt-10 text-center"
+          >
+            <Flex className="h-[90vh] w-full p-2" backgroundPosition="center center">
+              <VStack w={'full'} justify={'center'} px={px} p={2}>
+                <Stack maxW={'2xl'} align={'flex-start'} spacing={6} p={2}>
+                  <Heading as="h1" fontSize="9xl" fontWeight="extrabold" zIndex={3} color={'white'} p={2}>
+                    <Box w="100%" alignSelf="center">
+                      <m.div variants={varFade().inUp}>
+                        <ContentAlign>
+                          <Text
+                            id="decentralized-social-clearing-house"
+                            color={textColor}
+                            fontSize="6xl"
+                            fontWeight="bold"
+                            textAlign={'center'}
+                            zIndex={7}
+                          >
+                            A
+                            <br />
+                            <DecentralizedTextStyle>DECENTRALIZED</DecentralizedTextStyle>
+                            <br />
+                            SOCIAL CLEARING HOUSE
+                          </Text>
+                        </ContentAlign>
+                      </m.div>
+                      <Spacer h={10} />
+                      <m.div variants={varFade().in}>
+                        <chakra.div paddingLeft={{ base: 0, md: '30%' }}>
+                          <FlippingText textColor={textColor} />
+                        </chakra.div>
+                      </m.div>
+                      <br />
+                    </Box>
+                  </Heading>
+                </Stack>
+              </VStack>
+            </Flex>
+          </div>
+        )}
+      </AnimatePresence>
+      <Box id="social-issues-section" className="zmt-[-130px] overflow-hidden">
+        <AnimatePresence>
+          {isMd && (
+            <div className="landing-cards-zoom-in flex h-[90vh] flex-col justify-end">
+              <ParallaxText direction="right">
+                {dataSet1.map((item) => (
+                  <Card key={item.id} data={item} />
+                ))}
+                {dataSet1.map((item) => (
+                  <Card key={item.id} data={item} />
+                ))}
+              </ParallaxText>
+              <ParallaxText direction="left">
+                {dataSet2.map((item) => (
+                  <Card key={item.id} data={item} />
+                ))}
+                {dataSet2.map((item) => (
+                  <Card key={item.id} data={item} />
+                ))}
+              </ParallaxText>
+              <ParallaxText direction="right">
+                {dataSet3.map((item) => (
+                  <Card key={item.id} data={item} />
+                ))}
+                {dataSet3.map((item) => (
+                  <Card key={item.id} data={item} />
+                ))}
+              </ParallaxText>
+              <div className="flex-[1_0_auto]" />
+            </div>
+          )}
+        </AnimatePresence>
       </Box>
     </>
   );
